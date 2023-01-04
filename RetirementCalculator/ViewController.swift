27107c708df27f3ade86780ddca50cb737f3fafe
Nermin Ramić <nermin.ramic@asseco-see.ba>
2023-01-04 21:34:51 +0100

    Add feature 1

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -24,11 +24,16 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateButtonAction(_ sender: Any) {
-        let age = ageTextField.text ?? "NA"
-        let retirementAge = retirementAgeTextField.text ?? "NA"
+        let currentAge : Int? = Int(ageTextField.text!)
+        let plannedRetirementAge : Int? = Int(retirementAgeTextField.text!)
+        let monthlyInvestment : Float? = Float(monthlyInvestmentsTextField.text!)
+        let currentSavings : Float? = Float(savingsTextField.text!)
+        let interestRate: Float? = Float(interestRateTextField.text!)
         
-        let properties = ["age": age,
-                          "retirement_age" : retirementAge
+        resultLabel.text = "If you save $\(monthlyInvestment!) every month for \(plannedRetirementAge! - currentAge!) years, and invest that money plus your current investment of \(currentSavings!) at a \(interestRate!)% anual interest rate, you will have $X by the time you are \(currentAge!)"
+        
+        let properties = ["current_age": String(currentAge!),
+                          "planned_retirement_age" : String(plannedRetirementAge!)
         ]
         
         Analytics.trackEvent("calculate_retirement", withProperties: properties)

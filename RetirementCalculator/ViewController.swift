1385cf9d153c26e6590d4755693c6b7bdf74cf43
Nermin Ramić <nermin.ramic@asseco-see.ba>
2023-01-04 21:15:16 +0100

    Track calculate retirement event

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthlyInvestmentsTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var retirementAgeTextField: UITextField!
+    @IBOutlet weak var interestRateTextField: UITextField!
+    @IBOutlet weak var savingsTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func calculateButtonAction(_ sender: Any) {
+        let age = ageTextField.text ?? "NA"
+        let retirementAge = retirementAgeTextField.text ?? "NA"
+        
+        let properties = ["age": age,
+                          "retirement_age" : retirementAge
+        ]
+        
+        Analytics.trackEvent("calculate_retirement", withProperties: properties)
+    }
 }
 
